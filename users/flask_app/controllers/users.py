e7192bdb5d09506944fbfa9e01cd609b6e7fbc00
Tatiana Gutierrez <tatianagutierrezolivares15@gmail.com>
2023-07-03 17:35:11 -0400

    done

--- users/flask_app/controllers/users.py
+++ users/flask_app/controllers/users.py
@@ -1,8 +1,7 @@
-from flask import render_template, redirect, request, session, flash
+from flask import render_template, request, redirect
+
 from flask_app import app
 from flask_app.models.user import User
-
-app = Flask(__name__)
 
 
 @app.route("/")
